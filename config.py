b08eea5378a018dd8f621d40aec99380ded89cd2
Abel-Chu <72511296+Abel-Chu@users.noreply.github.com>
2021-01-15 02:20:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # imgur key
-client_id = 'e9247e67d390dd0'
-client_secret = 'ed35991e81f9c626679d098bb7becbbe36ecd830'
-album_id = 'a/u9qeUng'
-access_token = '67a47fc84c7c4a1921fcf22a7623dc47c7c35e6'
-refresh_token = '5624a2661ad67fa41bb2b91f306e7f56b0ba82a9'
+client_id = '你的client_id'
+client_secret = '你的client_secret'
+album_id = '你的album_id'
+access_token = '你的access_token'
+refresh_token = '你的refresh_token'
 
 # line bot key
-line_channel_access_token = 'bdMjodMq6Z+KTD2/J+oK+LjDdPro6a9K7ZtVVSZ8TFd3YbpAoCD5WrSq+TtEX8q7LxxHhjMcknukNctYEue4SiOJx3KVIjgQ7InEK0lhreHW5PTSFWQyxuq+9ayLRJlVwOzTdd/HM/mXxebCuxrrOQdB04t89/1O/w1cDnyilFU='
-line_channel_secret = '4f7bea5372827a5b94f6dfcde2fe130f'
+line_channel_access_token = '你的line_channel_access_token'
+line_channel_secret = '你的line_channel_secret'
